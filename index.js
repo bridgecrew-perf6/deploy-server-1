4e6b3ee07e8c5b1290cfcd9953bb142fbfe10bdb
Michael.Lu <Michael.Lu@grapecity.com>
2022-06-21 18:01:37 +0800

    listr add index.js

--- index.js
+++ index.js
@@ -3,54 +3,55 @@ import Listr from 'listr';
 import logger from "node-color-log";
 
 const tasks = new Listr([
-	{
-		title: 'Git',
-		task: () => {
-			return new Listr([
-				{
-					title: 'Checking git status',
-					task: () => execa('git', ['status', '--porcelain']).then(result => {
-						if (result.stdout !== '') {
-							throw new Error('Unclean working tree. Commit or stash changes first.');
-						}
-					})
-				},
-				{
-					title: 'Checking remote history',
-					task: () => execa('git', ['rev-list', '--count', '--left-only', '@{u}...HEAD']).then(result => {
-						if (result.stdout !== '0') {
-							throw new Error('Remote history differ. Please pull changes.');
-						}
-					})
-				}
-			], {concurrent: true});
-		}
-	},
-	{
-		title: 'Install package dependencies with Yarn',
-		task: (ctx, task) => execa('yarn')
-			.catch(() => {
-				ctx.yarn = false;
+    {
+        title: 'Git',
+        task: () => {
+            return new Listr([
+                {
+                    title: 'Checking git status',
+                    task: () => execa('git', ['status', '--porcelain']).then(result => {
+                        if (result.stdout !== '') {
+                            throw new Error('Unclean working tree. Commit or stash changes first.');
+                        }
+                    })
+                },
+                {
+                    title: 'Checking remote history',
+                    task: () => execa('git', ['rev-list', '--count', '--left-only', '@{u}...HEAD']).then(result => {
+                        if (result.stdout !== '0') {
+                            throw new Error('Remote history differ. Please pull changes.');
+                        }
+                    })
+                }
+            ], { concurrent: true });
+        }
+    },
+    {
+        title: 'Install package dependencies with Yarn',
+        task: (ctx, task) => execa('yarn')
+            .catch(() => {
+                ctx.yarn = false;
 
-				task.skip('Yarn not available, install it via `npm install -g yarn`');
-			})
-	},
-	{
-		title: 'Install package dependencies with npm',
-		enabled: ctx => ctx.yarn === false,
-		task: () => execa('npm', ['install'])
-	},
-	{
-		title: 'Run tests',
-		task: () => execa('npm', ['test'])
-	},
-	{
-		title: 'Publish package',
-		task: () => execa('npm', ['publish'])
-	}
+                task.skip('Yarn not available, install it via `npm install -g yarn`');
+            })
+    },
+    {
+        title: 'Install package dependencies with npm',
+        enabled: ctx => ctx.yarn === false,
+        task: () => execa('npm', ['install'])
+    },
+    {
+        title: 'Run tests',
+        task: () => execa('npm', ['test'])
+    },
+    {
+        title: 'Publish package',
+        task: () => execa('npm', ['publish'])
+    }
 ]);
 
 tasks.run().catch(err => {
     console.log("\r\n");
-	logger.error(err.toString());
+    console.log(err)
+    // logger.error(err.toString());
 });
